dac3409dcf1822bea0c2c20d7c5c87d250081eef
AshGoneMAD <ashokdy@gmail.com>
2018-08-03 15:31:34 +0400

    clean NEW

--- NYTimes/Model/Article.swift
+++ NYTimes/Model/Article.swift
@@ -18,6 +18,8 @@ struct Articles: Decodable {
     
     init(from decoder: Decoder) throws {
         //Container will map based on the CodingKeys
+        
+        
         let values = try decoder.container(keyedBy: CodingKeys.self)
         //To Get Array response for the given key - "results"
         var resultsArray = try values.nestedUnkeyedContainer(forKey: .results)
